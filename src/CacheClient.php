33fb51355e8438f2c81ee77c8d9a87dcec197570
b3none <ablackham2000@gmail.com>
2018-07-17 22:14:08 +0100

    Added the actual cache client.

---
+++ src/CacheClient.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace B3none\Cache;
+
+class CacheClient
+{
+    const CACHE_DIR = "/tmp/MelonCartel/cache";
+
+    /**
+     * @param string $id
+     * @param array $cacheValue
+     */
+    public function setCache(string $id, array $cacheValue) : void
+    {
+        if (!is_dir(self::CACHE_DIR)) {
+            mkdir(self::CACHE_DIR);
+        }
+
+        $cacheFile = self::CACHE_DIR . "/$id.json";
+        if (file_exists($cacheFile)) {
+            unlink($cacheFile);
+        }
+
+        $cacheValue['last-modified'] = time();
+
+        $file = fopen($cacheFile, "w");
+        fwrite($file, json_encode($cacheValue, JSON_PRETTY_PRINT));
+        fclose($file);
+    }
+
+    /**
+     * @param string $id
+     * @return bool
+     */
+    public function hasCache(string $id) : bool
+    {
+        $cacheFile = self::CACHE_DIR . "/$id.json";
+        return file_exists($cacheFile);
+    }
+
+    /**
+     * @param string $id
+     * @param int $minutes
+     * @return bool
+     * @throws \Exception
+     */
+    public function isFreshEnough(string $id, int $minutes = 30) : bool
+    {
+        $cachedData = $this->getCache($id);
+
+        if (!$cachedData) {
+            throw new \Exception("No cache file found.");
+        }
+        $difference = $cachedData['last-modified'] - time();
+        $difference = ($difference / 1000) / 60;
+
+        return round($difference <= $minutes);
+    }
+
+    /**
+     * @param string $id
+     * @return array
+     */
+    public function getCache(string $id) : array
+    {
+        $cacheFile = self::CACHE_DIR . "/$id.json";
+        return json_decode(file_get_contents($cacheFile), true);
+    }
+}
